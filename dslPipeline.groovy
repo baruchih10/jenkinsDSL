04eb327f08ec53641d8000916da810954541ecdf
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-27 17:51:11 +0200

    .

--- dslPipeline.groovy
+++ dslPipeline.groovy
@@ -49,6 +49,15 @@ pipeline {
       }
     }
     
+    stage('Login') {
+			steps {
+        withCredentials([[\$class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]
+        sh """
+          echo uname=\$USERNAME pwd=$PASSWORD
+        """ )
+			}
+		}
+
     stage('Build') {
       steps {
           echo 'Building... flaskImageBuild'
