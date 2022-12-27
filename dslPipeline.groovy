6e21e3efa0ce95cc3a6216e98268d002c911fa74
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-27 17:02:40 +0200

    .

--- dslPipeline.groovy
+++ dslPipeline.groovy
@@ -52,7 +52,7 @@ pipeline {
 			steps {
         withCredentials([[class: 'UsernamePasswordMultiBinding', credentialsId: 'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']])
         {
-          sh 'echo uname=$USERNAME pwd=$PASSWORD '
+          sh 'echo uname=${USERNAME} pwd=${PASSWORD} '
         }
 			}
 		}
