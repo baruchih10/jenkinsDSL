78be4d9933d32fc13f88f2b9c2ab34d3ac199e92
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-27 09:00:02 +0200

    .

--- dslPipelineSh.groovy
+++ dslPipelineSh.groovy
@@ -5,7 +5,7 @@ pipeline {
       steps {
         script {
           // Build and push the first Docker image
-          sh 'docker build -t bflask -f flask/Dockerfile'
+          sh 'docker build -t bflask ./flask'
           sh 'docker tag bflask:latest myusername/bflask:1.0'
           sh 'docker push myusername/bflask:1.0'
         }
@@ -16,7 +16,7 @@ pipeline {
         dependsOn 'Build and push flask'
         script {
           // Build and push the second Docker image
-          sh 'docker build -t bnginx -f nginx/Dockerfile '
+          sh 'docker build -t bnginx -f ./nginx'
           sh 'docker tag bnginx:latest myusername/bnginx:1.0'
           sh 'docker push myusername/bnginx:1.0'
         }
