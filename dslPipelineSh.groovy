8f32f2594b35de5cd72219fde50e623cee19695a
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-27 10:49:57 +0200

    .

--- dslPipelineSh.groovy
+++ dslPipelineSh.groovy
@@ -1,12 +1,18 @@
 pipeline {
   agent any
+  environment {
+    dockerhub = credentials('dockerhub')
+  }
+  
   stages {
+    stage('Login') {
+			steps {
+				sh 'echo $dockerhub_PWD | docker login -u $dockerhub_USR --password-stdin'
+			}
+		}
     stage('Build and push flask') {
       steps {
         script {
-          // Build and push the first Docker image
-          sh 'whoami > /tmp/who'
-          sh 'docker run hello-world'
           sh 'docker build -t bflask ./flask'
           sh 'docker tag bflask:latest bflask/bflask:1.0'
           sh 'docker push bflask/bflask:1.0'
@@ -17,7 +23,6 @@ pipeline {
       steps {
         dependsOn 'Build and push flask'
         script {
-          // Build and push the second Docker image
           sh 'docker build -t bnginx -f ./nginx'
           sh 'docker tag bnginx:latest bnginx/bnginx:1.0'
           sh 'docker push bnginx/bnginx:1.0'
