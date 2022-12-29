186c2b1ca403cf5ca81368368b7558b68fa79f4c
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-29 21:22:17 +0200

    .

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #support dynamic flask server:
 #envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
-cat /tmp/default.conf  | sed s/\$FLASK_SERVER_ADDR/${FLASK_SERVER_ADDR}/s > /etc/nginx/conf.d/default.conf 
+echo "FLASK_SERVER_ADDR env is: $FLASK_SERVER_ADDR"
+cat /tmp/default.conf  | sed s/\$FLASK_SERVER_ADDR/$FLASK_SERVER_ADDR/s > /etc/nginx/conf.d/default.conf 
  nginx -g 'daemon off;'
