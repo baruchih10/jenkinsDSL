344a36c405afd4305d70d199f066a23ff5d883a2
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-29 10:23:40 +0200

    .

--- flask/app.py
+++ flask/app.py
@@ -8,25 +8,10 @@ def hello():
 	return "Hello World!"
 
 @app.route('/containers')
-def get_containers():
-	output = subprocess.run(["docker", "ps"], stdout=subprocess.PIPE).stdout.decode('utf-8')
-	return output.split('\n')[1:]
-
-@app.route('/cache-me')
-def cache():
-	return "nginx will cache this response"
-
-@app.route('/info')
-def info():
-
-	resp = {
-		'connecting_ip': request.headers['X-Real-IP'],
-		'proxy_ip': request.headers['X-Forwarded-For'],
-		'host': request.headers['Host'],
-		'user-agent': request.headers['User-Agent']
-	}
-
-	return jsonify(resp)
+def list_containers():
+	client = docker.from_env()
+	containers = client.containers.list()
+	return jsonify([c.name for c in containers])
 
 @app.route('/flask-health-check')
 def flask_health_check():
--- nginx/start.sh
+++ nginx/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#support dynamic flask server:
 envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
