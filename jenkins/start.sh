beb1e9fc011afd5850c057cd151ae6f15cf69569
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-30 10:37:12 +0200

    .

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sed -i s/\$SERVER_IP/`hostname -I | cut -d' ' -f1`/ docker-compose.yml
-hostname -I | cut -d' ' -f1 > /tmp/JENKINS_SERVER_IP
+hostname -ן | cut -d' ' -f1 > /tmp/JENKINS_SERVER_IP
 docker-compose up
 
