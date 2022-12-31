03c0ff9a7e38bce526ef60fd8f9bf135829d4673
BaruchiHalamish20 <bhalamish20@gmail.com>
2022-12-31 19:57:56 +0200

    .

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-#support dynamic flask server:
-#envsubst '$FLASK_SERVER_ADDR' < /tmp/default.conf > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
-echo "JENKINS_SERVER_IP env is: $JENKINS_SERVER_IP"
-cat /tmp/default.conf  | sed s/FLASK_SERVER_ADDR/${JENKINS_SERVER_IP}/ > /etc/nginx/conf.d/default.conf 
+#support dynamic flask server based on predefine Environments :
+echo "FLASK env is: ${FLASK_SERVER_ADDR}:${FLASK_SERVER_PORT}"
+cat /tmp/default.conf  | sed s/FLASK_SERVER_ADDR/${FLASK_SERVER_ADDR}/ | sed s/FLASK_SERVER_PORT/${FLASK_SERVER_PORT}/ > /etc/nginx/conf.d/default.conf 
 nginx -g 'daemon off;'
